2cea0093a32c4a8cee50186930cc309b6cb3c3b4
kingstone <juwavictor@gmail.com>
2018-11-15 12:58:37 +0000

    product model added

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
 	'secret': 'apitesting',
-	'database': 'mongodb://noder:noderauth@proximus.modulusmongo.net:27017/so9pojyN'
+	'database': 'mongodb://topeshittu:success123@ds033699.mlab.com:33699/sales_mgt'
 
 };
